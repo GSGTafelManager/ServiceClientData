29a393418781c22e4e0cc8ed36681fc95b810d2e
GSGTafelManager <102914944+GSGTafelManager@users.noreply.github.com>
2022-04-06 21:13:00 +0200

    Update startup.py

--- startup.py
+++ startup.py
@@ -3,13 +3,15 @@ sentry_sdk.init("https://8b78151ad1da4102805eefb4a8fa8606@o1185419.ingest.sentry
 try:
     import pathlib
     import requests
-    code = requests.get("https://gsgtafelmanager.pythonanywhere.com/file/main.py").content
-    with open(str(pathlib.Path(__file__).parent / "main.pyw"), "wb") as f:
-        f.write(code)
+    import time
     while True:
         try:
+            code = requests.get("https://gsgtafelmanager.pythonanywhere.com/file/main.py").content
+            with open(str(pathlib.Path(__file__).parent / "main.pyw"), "wb") as f:
+                f.write(code)
             import main
         except:
+            time.sleep(10)
             continue
 except Exception as e:
     sentry_sdk.capture_exception(e)
