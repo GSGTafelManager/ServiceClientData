f0f28ca6f568f24aaa882fe155ac2b618572883e
GSGTafelManager <102914944+GSGTafelManager@users.noreply.github.com>
2022-04-06 16:55:09 +0200

    Update startup.py

--- startup.py
+++ startup.py
@@ -1,5 +1,5 @@
 import sentry_sdk
-sentry_sdk.init("https://8b78151ad1da4102805eefb4a8fa8606@o1185419.ingest.sentry.io/6303999", release="GSGTMServiceLauncher@1.0.0", traces_sample_rate=1.0)
+sentry_sdk.init("https://8b78151ad1da4102805eefb4a8fa8606@o1185419.ingest.sentry.io/6303999", release="GSGTMServiceLauncher@1.0.3", traces_sample_rate=1.0)
 try:
     import pathlib
     import requests
